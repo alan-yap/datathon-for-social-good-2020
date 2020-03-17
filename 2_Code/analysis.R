eda4a3c9e2e9c514cbee4f65f81f93bd6f3bdf55
Alan Yap <alan.yap@nousgroup.com.au>
2020-03-17 16:55:23 +1100

    Hub feature analysis updated with k-means clustering

--- 2_Code/analysis.R
+++ 2_Code/analysis.R
@@ -16,7 +16,26 @@ program_data_cube <- hub_raw %>%
 
 #----------Hub profile------------------
 
-# annual and average monthly participant count at each hub each year
+# status for each hub
+status <- clean_df %>% 
+  group_by(HubRandomID) %>% 
+  summarise(earliest_activity_date = min(ActivityDate),
+            latest_activity_date = max(ActivityDate)) %>% 
+  ungroup() %>% 
+  mutate(status_at_dec_19 = ifelse(latest_activity_date < ymd("2019-12-01"), 0, 1),
+         years_in_operation = round(time_length(interval(earliest_activity_date, latest_activity_date), "years"), digits = 1)) %>%
+  select(-earliest_activity_date, -latest_activity_date)
+
+# top three most common program offered at each hub each year 
+common_program <- clean_df %>% 
+  group_by(HubRandomID, year) %>% 
+  count(ProgrammeName, ProgrammeID) %>% 
+  arrange(desc(n)) %>%
+  slice(1:3) %>%
+  ungroup()
+
+
+# hub feature dataframe
 hub_feature <- clean_df %>% 
   group_by(HubRandomID, year) %>%
   #attendance features
@@ -54,25 +73,10 @@ hub_feature <- clean_df %>%
          avg_hub_annual_program_count = mean(annual_program_count),
          avg_hub_prtcpnt_per_activity = mean(avg_prtcpnt_per_activity),
          avg_hub_annual_volunteer = mean(annual_volunteer_all)) %>% 
-  ungroup()
-
-# top three most common program offered at each hub each year 
-common_program <- clean_df %>% 
-  group_by(HubRandomID, year) %>% 
-  count(ProgrammeName) %>% 
-  arrange(desc(n)) %>%
-  slice(1:3) %>%
-  ungroup()
-
-# status for each hub
-status <- clean_df %>% 
-  group_by(HubRandomID) %>% 
-  summarise(earliest_activity_date = min(ActivityDate),
-            latest_activity_date = max(ActivityDate)) %>% 
   ungroup() %>% 
-  mutate(status_at_dec_19 = ifelse(latest_activity_date < ymd("2019-12-01"), "Inactive", "Active"),
-         years_in_operation = round(time_length(interval(earliest_activity_date, latest_activity_date), "years"), digits = 1)) %>%
-  select(-earliest_activity_date, -latest_activity_date)
+  left_join(status, by = "HubRandomID")
+
+
 
 # top 10 hub in terms of annual participant size in 2019
 
@@ -95,7 +99,7 @@ bottom_prtcpnt_hub <- hub_feature %>%
 # averages for plotting horizontal line
 
 avg_yr_operation <- status %>% 
-  filter(status_at_dec_19 == "Active") %>% 
+  filter(status_at_dec_19 == 1) %>% 
   group_by(status_at_dec_19) %>% 
   summarise(avg_yrs_in_operation = mean(years_in_operation)) %>% 
   mutate(year = 2019) %>% 
@@ -109,140 +113,142 @@ avg_line <- hub_feature %>%
   left_join(avg_yr_operation, by = "year") %>% 
   select(-year)
 
-#-------------Clustering analysis----------------------------------
+#-------------k-means clustering for hubs----------------------------------
 
-# calculate distance of all hub features using a distance metric that can handle mixed data types: Gower distance
-# use only 2019 data
+# select a subset of features from hub_feature for clustering analysis. use 2019 observations
+# 
+# annual program count
+# annual activity count
+# annual prtcpnt all
+# annual volunteer all
+# status at dec 19
+# years in operation
 
+# scale data
+
+cluster_data <- hub_feature %>% 
+  select(HubRandomID, year, annual_program_count, annual_activity_count, annual_prtcpnt_all, annual_volunteer_all, status_at_dec_19, years_in_operation) %>% 
+  filter(year == 2019) %>% 
+  select(-year) %>% 
+  remove_rownames() %>% 
+  column_to_rownames(var = 'HubRandomID') 
+
+scale_data <- scale(cluster_data)
+
+# Optimal number of clusters in the data using the elbow method
+
+fviz_nbclust(scale_data, kmeans, method = "wss") +
+  geom_vline(xintercept = 5, linetype = 2)
+
+# Optimal number of clusters in the data using the silhouette method
+
+fviz_nbclust(scale_data, kmeans, method = "silhouette") 
+
+# start with 5 clusters. Compute k-means with k = 5
+ 
+set.seed(123)
+
+km.res <- kmeans(scale_data, 5, nstart = 25)
+
+print(km.res)
+
+# Compute the mean of each variable by clusters 
+
+aggregate(scale_data, by=list(cluster=km.res$cluster), mean)
+
+# assign cluster to original data
+
+cluster_data_final <- cbind(cluster_data, cluster = km.res$cluster)
+
+# visualise k-means clusters 
+
+fviz_cluster(km.res, cluster_data_final, ellipse.type = "norm")
+
+fviz_cluster(km.res, cluster_data_final,
+             palette = "Set2", ggtheme = theme_minimal())
+
+# assign cluster information to hub_feature and common program
+
+cluster_tmp <- cluster_data_final %>% 
+  rownames_to_column(var = "HubRandomID") %>%
+  mutate_if(is.character, as.numeric) %>% 
+  select(HubRandomID, cluster) 
+
+hub_feature_final <- cluster_tmp %>% 
+  right_join(hub_feature, by = "HubRandomID")
+
+common_program_final <- cluster_tmp %>% 
+  right_join(common_program, by = "HubRandomID")
+
+# # visualise k-means clusters by first reducing the number of features using PCA
+# # check that variables are highly correlated 
 # 
-# data <- feature_master %>% 
-#   filter(year == 2019) %>% 
-#   mutate_if(is.character, as.factor)
+# cormat <- round(cor(cluster_data_final), 2)
 # 
-# # remove hub id and year before clustering 
+# ggcorrplot(cormat, hc.order = TRUE, type = "lower", outline.color = "white")
 # 
-# gower_dist <- daisy(data[,-1:-2],
-#                     metric = "gower",
-#                     type = list(logratio = 3))
+# # PCA
+# # Scale data and identify principal components 
 # 
-# # Check attributes to ensure the correct methods are being used
-# # (I = interval, N = nominal)
-# # Note that despite logratio being called, 
-# # the type remains coded as "I"
+# pr_out <-prcomp(cluster_data_final, center = TRUE, scale = TRUE) 
 # 
-# summary(gower_dist)
+# summary(pr_out)
 # 
-# # Sanity check - Output most similar pair
+# # visualise principal components with a Screeplot and a cumulative PVE plot 
 # 
-# gower_mat <- as.matrix(gower_dist)
+# pr_var <-  pr_out$sdev ^ 2
 # 
-# data[
-#   which(gower_mat == min(gower_mat[gower_mat != min(gower_mat)]),
-#         arr.ind = TRUE)[1, ], ]
+# pve <- pr_var / sum(pr_var)
 # 
-# # Sanity check - Output most dissimilar pair
+# plot(pve, xlab = "Principal Component", ylab = "Proportion of Variance Explained", ylim = c(0,1), type = 'b')
 # 
-# data[
-#   which(gower_mat == max(gower_mat[gower_mat != max(gower_mat)]),
-#         arr.ind = TRUE)[1, ], ]
+# plot(cumsum(pve), xlab = "Principal Component", ylab = "Cumulative Proportion of Variance Explained", ylim =c(0,1), type = 'b')
 # 
-# # Determine the optimal number of clusters using the silhouette width metric
-# # Calculate silhouette width for many k using PAM
-# 
-# sil_width <- c(NA)
-# 
-# for(i in 2:10){
-#   
-#   pam_fit <- pam(gower_dist,
-#                  diss = TRUE,
-#                  k = i)
-#   
-#   sil_width[i] <- pam_fit$silinfo$avg.width
-#   
-# }
-# 
-# # Plot sihouette width (higher is better)
-# 
-# plot(1:10, sil_width,
-#      xlab = "Number of clusters",
-#      ylab = "Silhouette Width")
-# lines(1:10, sil_width)
-# 
-# # Run summary of 8 clusters 
-# 
-# pam_fit <- pam(gower_dist, diss = TRUE, k = 8)
-# 
-# pam_results <- data %>%
-#   mutate(cluster = pam_fit$clustering) %>%
-#   group_by(cluster) %>%
-#   do(the_summary = summary(.))
-# 
-# pam_results$the_summary
-# 
-# data[pam_fit$medoids, ]
-# 
-# # visualise using t-SNE 
-# 
-# tsne_obj <- Rtsne(gower_dist, is_distance = TRUE, perplexity = 1)
-# 
-# tsne_data <- tsne_obj$Y %>%
-#   data.frame() %>%
-#   setNames(c("X", "Y")) %>%
-#   mutate(cluster = factor(pam_fit$clustering),
-#          name = data$HubRandomID)
-# 
-# ggplot(aes(x = X, y = Y), data = tsne_data) +
-#   geom_point(aes(color = cluster))
-# 
-# #---------Clustering analysis 2--------------------
-# 
-# data2 <- feature_master %>% 
-#   select_if(is.numeric) 
-# 
-# scale_data2 <- scale(data2[,-1:-2]) 
-# 
-# data2_final <- data2 %>% 
-#   select(HubRandomID, year) %>% 
-#   merge(scale_data2)
+# # interpret the first two principal components before proceeding
+# # Rotate loadings
+# rot_loading <- varimax(pr_out$rotation[, 2:4])
+# rot_loading
+
+
 # 
 # #----------Volunteer------------------
 # 
-# volunteer$year <- factor(volunteer$year,
-#                          labels = c("2016", "2017", "2018", "2019"))
-# 
-# # Exploratory plots
-# 
-# p_school_volunteer <- volunteer %>%
-#   select(year, annual_school_volunteer) %>% 
-#   filter(annual_school_volunteer < quantile(annual_school_volunteer, 0.999)) %>% 
-#   ggplot(aes(x = year, y = annual_school_volunteer)) +
-#   geom_boxplot() +
-#   scale_y_continuous(breaks = seq(0, 1000, 100),
-#                      limits = c(0, 1000))
-# 
-# p_hub_volunteer <- volunteer %>%
-#   select(year, annual_hub_volunteer) %>% 
-#   filter(annual_hub_volunteer < quantile(annual_hub_volunteer, 0.999)) %>% 
-#   ggplot(aes(x = year, y = annual_hub_volunteer)) +
-#   geom_boxplot() +
-#   scale_y_continuous(breaks = seq(0, 1000, 100),
-#                      limits = c(0, 1000))
-# 
-# p_external_volunteer <- volunteer %>%
-#   select(year, annual_external_volunteer) %>% 
-#   filter(annual_external_volunteer < quantile(annual_external_volunteer, 0.999)) %>% 
-#   ggplot(aes(x = year, y = annual_external_volunteer)) +
-#   geom_boxplot() +
-#   scale_y_continuous(breaks = seq(0, 1000, 100),
-#                      limits = c(0, 1000))
-# 
-# p_all_volunteer <- volunteer %>%
-#   select(year, annual_volunteer_all) %>% 
-#   filter(annual_volunteer_all < quantile(annual_volunteer_all, 0.999)) %>% 
-#   ggplot(aes(x = year, y = annual_volunteer_all)) +
-#   geom_boxplot() +
-#   scale_y_continuous(breaks = seq(0, 1500, 100),
-#                      limits = c(0, 1500))
+hub_feature_final$year <- factor(hub_feature_final$year,
+                         labels = c("2016", "2017", "2018", "2019"))
+
+# Exploratory plots
+
+p_school_volunteer <- hub_feature_final %>%
+  select(year, annual_school_volunteer) %>%
+  filter(annual_school_volunteer < quantile(annual_school_volunteer, 0.999)) %>%
+  ggplot(aes(x = year, y = annual_school_volunteer)) +
+  geom_boxplot() +
+  scale_y_continuous(breaks = seq(0, 800, 100),
+                     limits = c(0, 800))
+
+p_hub_volunteer <- hub_feature_final %>%
+  select(year, annual_hub_volunteer) %>%
+  filter(annual_hub_volunteer < quantile(annual_hub_volunteer, 0.999)) %>%
+  ggplot(aes(x = year, y = annual_hub_volunteer)) +
+  geom_boxplot() +
+  scale_y_continuous(breaks = seq(0, 1000, 100),
+                     limits = c(0, 1000))
+
+p_external_volunteer <- hub_feature_final %>%
+  select(year, annual_external_volunteer) %>%
+  filter(annual_external_volunteer < quantile(annual_external_volunteer, 0.999)) %>%
+  ggplot(aes(x = year, y = annual_external_volunteer)) +
+  geom_boxplot() +
+  scale_y_continuous(breaks = seq(0, 1000, 100),
+                     limits = c(0, 1000))
+
+p_all_volunteer <- hub_feature_final %>%
+  select(year, annual_volunteer_all) %>%
+  filter(annual_volunteer_all < quantile(annual_volunteer_all, 0.999)) %>%
+  ggplot(aes(x = year, y = annual_volunteer_all)) +
+  geom_boxplot() +
+  scale_y_continuous(breaks = seq(0, 1500, 100),
+                     limits = c(0, 1500))
 
 
 
